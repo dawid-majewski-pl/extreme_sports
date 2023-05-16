fabf94e9a64438d20a175889b0037147c344b8be
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-05-16 21:56:12 +0200

    Working Hamburger menu with animation and  js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,19 @@ module.exports = {
         'orange-main': '#f9c22e',
         'orange-second': '#ffa029',
         'gunmetal-main': '#1f2b37',
+      },
+      opacity: {
+        'barely-visible': '0.98'
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        }
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
