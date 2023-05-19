4facd127d21dffdb9526066e4a15bde8ec61e374
Dawid Majewski <majewski.dawid.91@gmail.com>
2023-05-19 22:24:56 +0200

    Working gallery.

--- public/main.js
+++ public/main.js
@@ -13,3 +13,29 @@ const initApp = () => {
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
+
+const radioBtn1 = document.querySelector('#radio1')
+const radioBtn2 = document.querySelector('#radio2')
+const radioBtn3 = document.querySelector('#radio3')
+const radioBtn4 = document.querySelector('#radio4')
+const content = document.querySelector('#container')
+
+radioBtn1.addEventListener('click', () => {
+    console.log('conteiner')
+    content.innerHTML = '<img src="img/slider/skateboarder.jpg" alt="Photo of skateboarder.">'
+})
+
+radioBtn2.addEventListener('click', () => {
+    console.log('container')
+    content.innerHTML = '<img src="img/slider/snowboarder.jpg" alt="Photo of snowboarder.">'
+})
+
+radioBtn3.addEventListener('click', () => {
+    console.log('container')
+    content.innerHTML = '<img src="img/slider/biker.jpg" alt="Photo of biker.">'
+})
+
+radioBtn4.addEventListener('click', () => {
+    console.log('container')
+    content.innerHTML = '<img src="img/slider/rider.jpg" alt="Photo of rider.">'
+})
